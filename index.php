38527266820b979c973dedd88b76bed971ba8068
Scott Boms <scottboms@gmail.com>
2025-07-15 16:06:07 -0700

    A little code cleanup/formatting.

--- index.php
+++ index.php
@@ -19,35 +19,35 @@ if (Semver::satisfies(Kirby::version() ?? '0.0.0', '~4.0 || ~5.0') === false) {
 }
 
 Kirby::plugin(
-  name: 'scottboms/applemusic-field', 
-  info: [
-    'homepage' => 'https://github.com/scottboms/kirby-applemusic'
-  ],
-  version: '1.1.0',
-  license: 'MIT',
-  extends: [
-    'options' => [
-      'format' => 'link' // fallback if not defined in config.php
-    ],
-    'fields' => [
-      'applemusic' => [
-        'computed' => [
-          'format' => function () {
-            return option('scottboms.applemusic.format', 'embed');
-          }
-        ],
-        'props' => [
-          'label' => function( $label = 'Apple Music Embed') {
-            return $label;
-          },
-          'help' => function( $help = null) {
-            return $help;
-          }
-        ]
-      ]
-    ],
-    'snippets' => [
-      'applemusic' => __DIR__ . '/snippets/applemusic.php'
-    ]
-  ]
+	name: 'scottboms/applemusic-field',
+	info: [
+		'homepage' => 'https://github.com/scottboms/kirby-applemusic'
+	],
+	version: '1.1.0',
+	license: 'MIT',
+	extends: [
+		'options' => [
+			'format' => 'link' // fallback if not defined in config.php
+		],
+		'fields' => [
+			'applemusic' => [
+				'computed' => [
+					'format' => function () {
+						return option('scottboms.applemusic.format', 'embed');
+					}
+				],
+				'props' => [
+					'label' => function( $label = 'Apple Music Embed') {
+						return $label;
+					},
+					'help' => function( $help = null) {
+						return $help;
+					}
+				]
+			]
+		],
+		'snippets' => [
+			'applemusic' => __DIR__ . '/snippets/applemusic.php'
+		]
+	]
 );
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import AppleMusicField from "./components/AppleMusicField.vue";
 import { icons } from "./icons.js";
 
 panel.plugin("scottboms/kirby-applemusic", {
-  icons,
-  fields: {
-    applemusic: AppleMusicField
-  }
+	icons,
+	fields: {
+		applemusic: AppleMusicField
+	}
 });
