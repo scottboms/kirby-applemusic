de76197180b810213d0129f11a8c93ccaf40e36e
Scott Boms <scottboms@gmail.com>
2025-12-10 23:29:56 -0800

    Initial check for Kirby 6 dev.

--- index.php
+++ index.php
@@ -24,8 +24,8 @@ use Scottboms\MusicKit\Utils;
 
 // shamelessly borrowed from distantnative/retour-for-kirby
 if (
-	version_compare(App::version() ?? '0.0.0', '4.0.1', '<') === true ||
-	version_compare(App::version() ?? '0.0.0', '6.0.0', '>=') === true
+	version_compare(App::version() ?? '0.0.0', '5.0.0', '<') === true ||
+	version_compare(App::version() ?? '6.0.0', '7.0.0', '>=') === true
 ) {
 	throw new Exception('Apple Music requires Kirby v4 or v5');
 }
